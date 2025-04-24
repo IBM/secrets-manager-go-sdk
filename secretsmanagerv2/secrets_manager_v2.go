e36b2573cd8f608597fe37c389aa2a76212ae623
Avi Ribchinsky <avir@il.ibm.com>
2025-04-24 14:25:02 +0300

    Adding support for custom credentials.

--- secretsmanagerv2/secrets_manager_v2.go
+++ secretsmanagerv2/secrets_manager_v2.go
@@ -1349,9 +1349,13 @@ func (secretsManager *SecretsManagerV2) GetSecretVersionWithContext(ctx context.
 }
 
 // DeleteSecretVersionData : Delete the data of a secret version
-// Delete the data of a secret version by specifying the ID of the version.
+// Delete the data of a secret version by specifying its version ID.
 //
-// This operation is available for secret type: iam_credentials current version.
+// This operation is available for the following secret types:
+//
+// `iam_credentials`: current version only
+//
+// `custom_credentials`: both current and previous versions.
 func (secretsManager *SecretsManagerV2) DeleteSecretVersionData(deleteSecretVersionDataOptions *DeleteSecretVersionDataOptions) (response *core.DetailedResponse, err error) {
 	response, err = secretsManager.DeleteSecretVersionDataWithContext(context.Background(), deleteSecretVersionDataOptions)
 	err = core.RepurposeSDKProblem(err, "")
